4eda217c5a9d2cc39ec6cc73b44a53082dca7f62
FIN443 <70796031+FIN443@users.noreply.github.com>
2021-11-29 15:56:29 +0900

    week 4-2

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -21,6 +21,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -29,6 +31,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.samples.apps.sunflower.R
@@ -106,6 +109,204 @@ class PlantDetailFragment : Fragment() {
                     else -> false
                 }
             }
+
+            composeView.apply {
+                // Dispose the Composition when the view's LifecycleOwner
+                // is destroyed@Preview
+                //@Composable
+                //private fun PlantDetailContentPreview() {
+                //    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+                //    MdcTheme {
+                //        PlantDetailContent(plant)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantNamePreview() {
+                //    MdcTheme {
+                //        PlantName("Apple")
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantWateringPreview() {
+                //    MdcTheme {
+                //        PlantWatering(7)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantDescriptionPreview() {
+                //    MdcTheme {
+                //        PlantDescription("HTML<br><br>description")
+                //    }
+                //}@Preview
+                //@Composable
+                //private fun PlantDetailContentPreview() {
+                //    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+                //    MdcTheme {
+                //        PlantDetailContent(plant)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantNamePreview() {
+                //    MdcTheme {
+                //        PlantName("Apple")
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantWateringPreview() {
+                //    MdcTheme {
+                //        PlantWatering(7)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantDescriptionPreview() {
+                //    MdcTheme {
+                //        PlantDescription("HTML<br><br>description")
+                //    }
+                //}@Preview
+                //@Composable
+                //private fun PlantDetailContentPreview() {
+                //    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+                //    MdcTheme {
+                //        PlantDetailContent(plant)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantNamePreview() {
+                //    MdcTheme {
+                //        PlantName("Apple")
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantWateringPreview() {
+                //    MdcTheme {
+                //        PlantWatering(7)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantDescriptionPreview() {
+                //    MdcTheme {
+                //        PlantDescription("HTML<br><br>description")
+                //    }
+                //}@Preview
+                //@Composable
+                //private fun PlantDetailContentPreview() {
+                //    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+                //    MdcTheme {
+                //        PlantDetailContent(plant)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantNamePreview() {
+                //    MdcTheme {
+                //        PlantName("Apple")
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantWateringPreview() {
+                //    MdcTheme {
+                //        PlantWatering(7)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantDescriptionPreview() {
+                //    MdcTheme {
+                //        PlantDescription("HTML<br><br>description")
+                //    }
+                //}@Preview
+                //@Composable
+                //private fun PlantDetailContentPreview() {
+                //    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+                //    MdcTheme {
+                //        PlantDetailContent(plant)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantNamePreview() {
+                //    MdcTheme {
+                //        PlantName("Apple")
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantWateringPreview() {
+                //    MdcTheme {
+                //        PlantWatering(7)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantDescriptionPreview() {
+                //    MdcTheme {
+                //        PlantDescription("HTML<br><br>description")
+                //    }
+                //}@Preview
+                //@Composable
+                //private fun PlantDetailContentPreview() {
+                //    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+                //    MdcTheme {
+                //        PlantDetailContent(plant)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantNamePreview() {
+                //    MdcTheme {
+                //        PlantName("Apple")
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantWateringPreview() {
+                //    MdcTheme {
+                //        PlantWatering(7)
+                //    }
+                //}
+                //
+                //@Preview
+                //@Composable
+                //private fun PlantDescriptionPreview() {
+                //    MdcTheme {
+                //        PlantDescription("HTML<br><br>descript
+                setViewCompositionStrategy(
+                    ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+                )
+                setContent {
+                    MdcTheme {
+                        PlantDetailDescription(plantDetailViewModel)
+                    }
+                }
+            }
+
         }
         setHasOptionsMenu(true)
 
